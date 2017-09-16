4891a81015bc8c24e739acffd6ce6c8daf03c378
Filip Hrisafov <filip.hrisafov@gmail.com>
2017-09-16 14:25:25 +0200

    Copy mapstruct dependncy into build folder and use it during tests

--- src/test/java/org/mapstruct/intellij/MapstructBaseCompletionTestCase.java
+++ src/test/java/org/mapstruct/intellij/MapstructBaseCompletionTestCase.java
@@ -18,10 +18,16 @@
  */
 package org.mapstruct.intellij;
 
+import java.io.File;
+
 import com.intellij.codeInsight.completion.LightFixtureCompletionTestCase;
 import com.intellij.openapi.util.text.StringUtil;
-import org.intellij.lang.annotations.Language;
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
+import com.intellij.testFramework.PsiTestUtil;
+import com.intellij.util.PathUtil;
 import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.testFramework.LightPlatformTestCase.getModule;
 
 /**
  * Base completion test case for MapStruct.
@@ -30,112 +36,15 @@ import org.jetbrains.annotations.NotNull;
  */
 public abstract class MapstructBaseCompletionTestCase extends LightFixtureCompletionTestCase {
 
+    private static final String BUILD_LIBS_DIRECTORY = "build/libs";
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        addEnvironmentClasses();
-    }
-
-    protected void addEnvironmentClasses() {
-        //TODO maybe there is a better way to do this. Maybe unpack the dependency somewhere in build
-        // and add the entire API.
-        //CHECKSTYLE:OFF
-        //@formatter:off
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "public enum ReportingPolicy {\n" +
-            "    IGNOREPORTING_POLICY,\n" +
-            "    WARN,\n" +
-            "    ERROR;\n" +
-            "}" );
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "public enum CollectionMappingStrategy {\n" +
-            "    ACCESSOR_ONLY,\n" +
-            "    SETTER_PREFERRED,\n" +
-            "    ADDER_PREFERRED,\n" +
-            "    TARGET_IMMUTABLE;\n" +
-            "}" );
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "public enum NullValueMappingStrategy {\n" +
-            "    RETURN_NULL,\n" +
-            "    RETURN_DEFAULT;\n" +
-            "}" );
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "public enum MappingInheritanceStrategy {\n" +
-            "    EXPLICIT,\n" +
-            "    AUTO_INHERIT_FROM_CONFIG;\n" +
-            "}" );
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "public enum NullValueCheckStrategy {\n" +
-            "    ON_IMPLICIT_CONVERSION,\n" +
-            "    ALWAYS;\n" +
-            "}" );
-
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "import static org.mapstruct.NullValueCheckStrategy.ON_IMPLICIT_CONVERSION;\n" +
-            "public @interface Mapper {\n" +
-            "    Class<?>[] uses() default { };\n" +
-            "    Class<?>[] imports() default { };\n" +
-            "    ReportingPolicy unmappedTargetPolicy() default ReportingPolicy.WARN;\n" +
-            "    String componentModel() default \"default\";\n" +
-            "    String implementationName() default \"<CLASS_NAME>Impl\";\n" +
-            "    String implementationPackage() default \"<PACKAGE_NAME>\";\n" +
-            "    Class<?> config() default void.class;\n" +
-            "    CollectionMappingStrategy collectionMappingStrategy() default CollectionMappingStrategy.ACCESSOR_ONLY;\n" +
-            "    NullValueMappingStrategy nullValueMappingStrategy() default NullValueMappingStrategy.RETURN_NULL;\n" +
-            "    MappingInheritanceStrategy mappingInheritanceStrategy() default MappingInheritanceStrategy.EXPLICIT;" +
-            "    NullValueCheckStrategy nullValueCheckStrategy() default ON_IMPLICIT_CONVERSION;\n" +
-            "    boolean disableSubMappingMethodsGeneration() default false;\n" +
-            "}" );
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "import static org.mapstruct.NullValueCheckStrategy.ON_IMPLICIT_CONVERSION;\n" +
-            "public @interface MapperConfig {\n" +
-            "    Class<?>[] uses() default { };\n" +
-            "    ReportingPolicy unmappedTargetPolicy() default ReportingPolicy.WARN;\n" +
-            "    String componentModel() default \"default\";\n" +
-            "    String implementationName() default \"<CLASS_NAME>Impl\";\n" +
-            "    String implementationPackage() default \"<PACKAGE_NAME>\";\n" +
-            "    CollectionMappingStrategy collectionMappingStrategy() default CollectionMappingStrategy.ACCESSOR_ONLY;\n" +
-            "    NullValueMappingStrategy nullValueMappingStrategy() default NullValueMappingStrategy.RETURN_NULL;\n" +
-            "    MappingInheritanceStrategy mappingInheritanceStrategy() default MappingInheritanceStrategy.EXPLICIT;" +
-            "    NullValueCheckStrategy nullValueCheckStrategy() default ON_IMPLICIT_CONVERSION;\n" +
-            "    boolean disableSubMappingMethodsGeneration() default false;\n" +
-            "}" );
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "import java.lang.annotation.Annotation;\n" +
-            "public @interface Mapping {\n" +
-            "    String target();\n" +
-            "    String source() default \"\";\n" +
-            "    String dateFormat() default \"\";\n" +
-            "    String numberFormat() default \"\";\n" +
-            "    String constant() default \"\";\n" +
-            "    String expression() default \"\";\n" +
-            "    boolean ignore() default false;\n" +
-            "    Class<? extends Annotation>[] qualifiedBy() default { };\n" +
-            "    String[] qualifiedByName() default { };\n" +
-            "    Class<?> resultType() default void.class;\n" +
-            "    String[] dependsOn() default { };\n" +
-            "    String defaultValue() default \"\";\n" +
-            "}" );
-
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "import java.lang.annotation.Annotation;\n" +
-            "public @interface ValueMapping {\n" +
-            "    String target();\n" +
-            "    String source();\n" +
-            "}" );
-
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "public @interface MappingTarget {\n" +
-            "}" );
-        addEnvironmentClass( "package org.mapstruct;\n" +
-            "public @interface Context {\n" +
-            "}" );
-        //@formatter:on
-        //CHECKSTYLE:ON
-    }
-
-    protected void addEnvironmentClass(@Language("JAVA") @NotNull String classText) {
-        myFixture.addClass( classText );
+        final String mapstructLibPath = PathUtil.toSystemIndependentName( new File( BUILD_LIBS_DIRECTORY )
+            .getAbsolutePath() );
+        VfsRootAccess.allowRootAccess( mapstructLibPath );
+        PsiTestUtil.addLibrary( getModule(), "Mapstruct", mapstructLibPath, "mapstruct.jar" );
     }
 
     protected void addDirectoryToProject(@NotNull String directory) {
