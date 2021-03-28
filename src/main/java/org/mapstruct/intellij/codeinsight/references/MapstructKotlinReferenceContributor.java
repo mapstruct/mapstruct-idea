a1ec88a8771206b337bfa35430cc0735d132e8ab
Filip Hrisafov <filip.hrisafov@gmail.com>
2021-03-28 11:28:32 +0200

    Replace kotlin reference contributor written in kotlin with one in java
    There is some breaking change in Kotlin that causes the kotlin code to throw an exception when used in earlier IntelliJ versions

---
+++ src/main/java/org/mapstruct/intellij/codeinsight/references/MapstructKotlinReferenceContributor.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.intellij.codeinsight.references;
+
+import com.intellij.psi.PsiReferenceContributor;
+import com.intellij.psi.PsiReferenceRegistrar;
+import org.jetbrains.annotations.NotNull;
+
+import static org.mapstruct.intellij.util.MapstructKotlinElementUtils.mappingElementPattern;
+import static org.mapstruct.intellij.util.MapstructKotlinElementUtils.valueMappingElementPattern;
+
+/**
+ * {@link PsiReferenceContributor} for MapStruct annotations in Kotlin code.
+ *
+ * @author Frank Wang
+ * @author Filip Hrisafov
+ */
+public class MapstructKotlinReferenceContributor extends PsiReferenceContributor {
+
+    @Override
+    public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
+        registrar.registerReferenceProvider(
+            mappingElementPattern( "target" ),
+            new MappingTargetReferenceProvider( MapstructTargetReference::create )
+        );
+        registrar.registerReferenceProvider(
+            mappingElementPattern( "source" ),
+            new MappingTargetReferenceProvider( MapstructSourceReference::create )
+        );
+        registrar.registerReferenceProvider(
+            valueMappingElementPattern( "target" ),
+            new MappingTargetReferenceProvider( ValueMappingSourceReference::create )
+        );
+        registrar.registerReferenceProvider(
+            valueMappingElementPattern( "source" ),
+            new MappingTargetReferenceProvider( ValueMappingTargetReference::create )
+        );
+    }
+
+}
