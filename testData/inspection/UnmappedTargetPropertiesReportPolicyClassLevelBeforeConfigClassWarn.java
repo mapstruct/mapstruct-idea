59026197610f7d27f12cdc122961d35e753206ae
hduelme <46139144+hduelme@users.noreply.github.com>
2023-05-08 08:50:26 +0200

    Use https links (#139)

--- testData/inspection/UnmappedTargetPropertiesReportPolicyClassLevelBeforeConfigClassWarn.java
+++ testData/inspection/UnmappedTargetPropertiesReportPolicyClassLevelBeforeConfigClassWarn.java
@@ -1,7 +1,7 @@
 package inspection;/*
  * Copyright MapStruct Authors.
  *
- * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License version 2.0, available at https://www.apache.org/licenses/LICENSE-2.0
  */
 
 import org.example.data.UnmappedTargetPropertiesData.Source;
