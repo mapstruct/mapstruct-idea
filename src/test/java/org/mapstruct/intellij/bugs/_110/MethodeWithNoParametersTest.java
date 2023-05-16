2115af520395751fea4db3c72ef96665bc7d39ab
hdulme <hduelme@gmx.de>
2023-05-16 09:15:59 +0200

    fix IndexOutOfBoundsException for target methods without parameter

---
+++ src/test/java/org/mapstruct/intellij/bugs/_110/MethodeWithNoParametersTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at https://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.intellij.bugs._110;
+
+import org.mapstruct.intellij.MapstructBaseCompletionTestCase;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author hdulme
+ */
+public class MethodeWithNoParametersTest extends MapstructBaseCompletionTestCase {
+
+    @Override
+    protected String getTestDataPath() {
+        return "testData/bugs/_110";
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        myFixture.copyFileToProject( "TargetNoPropertyMethode.java",
+                "org/example/TargetNoPropertyMethode.java" );
+    }
+
+    public void testExpressionCompletion() {
+        myFixture.configureByFile( "MapExpression.java" );
+        assertThat( myFixture.completeBasic() ).isEmpty();
+    }
+
+    public void testDefaultExpressionCompletion() {
+        myFixture.configureByFile( "MapDefaultExpression.java" );
+        assertThat( myFixture.completeBasic() ).isEmpty();
+    }
+
+    public void testTargetCompletion() {
+        myFixture.configureByFile( "TargetCompletion.java" );
+        assertThat( myFixture.completeBasic() ).isEmpty();
+    }
+}
---
+++ testData/bugs/_110/MapExpression.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at https://www.apache.org/licenses/LICENSE-2.0
+ */
+import org.mapstruct.*;
+import org.example.TargetNoPropertyMethode;
+
+@Mapper
+public interface MapExpression {
+
+    @Mapping(target = "active", expression = "java(<caret>)")
+    TargetNoPropertyMethode mapExpression(String string)
+}
