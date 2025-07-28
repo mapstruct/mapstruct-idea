e08b1371e6007453ecdaa327e190cdf000fa3968
thunderhook <8238759+thunderhook@users.noreply.github.com>
2025-07-28 08:18:22 +0200

    #236: Add a test for mapping composition using the @Mappings wrapper annotation

--- testData/bugs/_236/DisableSourceAndTargetPropertyInspectionOnAnnotations.java
+++ testData/bugs/_236/DisableSourceAndTargetPropertyInspectionOnAnnotations.java
@@ -8,9 +8,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Retention(RetentionPolicy.CLASS)
 @Mapping(target = "id", ignore = true)
 @Mapping(target = "creationDate", expression = "java(new java.util.Date())")
 @Mapping(target = "name", source = "groupName")
 @interface ToEntity { }
+
+@Retention(RetentionPolicy.CLASS)
+@Mappings({
+    @Mapping(target = "id", ignore = true),
+    @Mapping(target = "creationDate", expression = "java(new java.util.Date())"),
+    @Mapping(target = "name", source = "groupName")
+})
+@interface ToEntityWithMappings { }
+
